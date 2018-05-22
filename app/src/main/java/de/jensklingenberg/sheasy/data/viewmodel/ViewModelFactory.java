f29894272b1ae4f2a8baa0df000e7ae01ff3ee8f
Jens Klingenberg <beispiel@posteo.de>
2018-05-22 23:33:15 +0200

    test github pages

---
+++ app/src/main/java/de/jensklingenberg/sheasy/data/viewmodel/ViewModelFactory.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2017 Google Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package de.jensklingenberg.sheasy.data.viewmodel;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.app.FragmentActivity;
+
+public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    @SuppressLint("StaticFieldLeak")
+    private static volatile ViewModelFactory INSTANCE;
+
+    private final Application mApplication;
+
+
+    private ViewModelFactory(Application application) {
+        mApplication = application;
+
+    }
+
+    public static ViewModelFactory getInstance(Application application) {
+
+        if (INSTANCE == null) {
+            synchronized (ViewModelFactory.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ViewModelFactory(application);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    @VisibleForTesting
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+    public static ProfileViewModel obtainProfileViewModel(FragmentActivity activity) {
+        // Use a Factory to inject dependencies into the ViewModel
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+        return ViewModelProviders.of(activity, factory).get(ProfileViewModel.class);
+    }
+
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(ProfileViewModel.class)) {
+            //noinspection unchecked
+            return (T) new ProfileViewModel(mApplication);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+    }
+
+
+}
